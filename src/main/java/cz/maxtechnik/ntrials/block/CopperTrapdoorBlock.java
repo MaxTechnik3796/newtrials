dabb8da40ac987d3367526386f70e54ddf81e344
Adam Bartoš <programingmaster147@gmail.com>
2025-08-25 16:35:33 +0200

    add axe scraping on copper trapdooors

--- src/main/java/cz/maxtechnik/ntrials/block/CopperTrapdoorBlock.java
+++ src/main/java/cz/maxtechnik/ntrials/block/CopperTrapdoorBlock.java
@@ -83,38 +83,40 @@ public class CopperTrapdoorBlock extends TrapDoorBlock implements WeatheringCopp
                 }
                 return InteractionResult.sidedSuccess(level.isClientSide);
             }
-            //axe scraping
-            if (itemInHand.getItem()instanceof AxeItem){
-                Block scrapedBlock=NTrialsModEvents.SCRAPING_MAP.get(this);
-                if(scrapedBlock!=null){ // Null znamená že je to první fáze (nelze čistit dál)
-                    if(!level.isClientSide){
-                        BlockState stateAtPos = level.getBlockState(pos);
-                        // Zachováváme orientáciu a stav trapdoor
-                        BlockState nextState= waxedBlock.defaultBlockState()
-                                .setValue(FACING, stateAtPos.getValue(FACING))
-                                .setValue(OPEN, stateAtPos.getValue(OPEN))
-                                .setValue(HALF, stateAtPos.getValue(HALF))
-                                .setValue(POWERED, stateAtPos.getValue(POWERED))
-                                .setValue(WATERLOGGED, stateAtPos.getValue(WATERLOGGED));
 
-                        level.setBlock(pos,nextState,3);
+        }
+        //axe scraping
+        if (itemInHand.getItem() instanceof AxeItem){
+            Block scrapedBlock=NTrialsModEvents.SCRAPING_MAP.get(this);
+            if(scrapedBlock!=null){ // Null znamená že je to první fáze (nelze čistit dál)
+                if(!level.isClientSide){
+                    BlockState stateAtPos = level.getBlockState(pos);
+                    // Zachováváme orientáciu a stav trapdoor
+                    BlockState nextState = scrapedBlock.defaultBlockState()
+                            .setValue(FACING, stateAtPos.getValue(FACING))
+                            .setValue(OPEN, stateAtPos.getValue(OPEN))
+                            .setValue(HALF, stateAtPos.getValue(HALF))
+                            .setValue(POWERED, stateAtPos.getValue(POWERED))
+                            .setValue(WATERLOGGED, stateAtPos.getValue(WATERLOGGED));
 
-                        level.playSound(null,pos,SoundEvents.AXE_SCRAPE,SoundSource.BLOCKS,1.0f,1.0f);
-                        if(level instanceof ServerLevel serverLevel){
-                            for(int i=0;i<20;i++){
-                                double x=pos.getX()-0.2+level.random.nextDouble()*1.4;
-                                double y=pos.getY()-0.2+level.random.nextDouble()*1.4;
-                                double z=pos.getZ()-0.2+level.random.nextDouble()*1.4;
-                                serverLevel.sendParticles(ParticleTypes.SCRAPE,x,y,z,1,0,0,0,0.05);
-                            }
+                    level.setBlock(pos,nextState,3);
+
+                    level.playSound(null,pos,SoundEvents.AXE_SCRAPE,SoundSource.BLOCKS,1.0f,1.0f);
+                    if(level instanceof ServerLevel serverLevel){
+                        for(int i=0;i<20;i++){
+                            double x=pos.getX()-0.2+level.random.nextDouble()*1.4;
+                            double y=pos.getY()-0.2+level.random.nextDouble()*1.4;
+                            double z=pos.getZ()-0.2+level.random.nextDouble()*1.4;
+                            serverLevel.sendParticles(ParticleTypes.SCRAPE,x,y,z,1,0,0,0,0.05);
                         }
-                        // Poškodenie nástroja
-                        itemInHand.hurtAndBreak(1,player,(p)->p.broadcastBreakEvent(hand));
                     }
-                    return InteractionResult.sidedSuccess(level.isClientSide);
+                    // Poškodenie nástroja
+                    itemInHand.hurtAndBreak(1,player,(p)->p.broadcastBreakEvent(hand));
                 }
+                return InteractionResult.sidedSuccess(level.isClientSide);
             }
         }
+
         return super.use(state,level,pos,player,hand,hit);
     }
 
