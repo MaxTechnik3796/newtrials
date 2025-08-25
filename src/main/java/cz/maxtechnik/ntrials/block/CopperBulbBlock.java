e0a7413b755f5c17b9b40222d44de05ddb2dadd3
Adam Bartoš <programingmaster147@gmail.com>
2025-08-25 21:07:15 +0200

    add if statement

--- src/main/java/cz/maxtechnik/ntrials/block/CopperBulbBlock.java
+++ src/main/java/cz/maxtechnik/ntrials/block/CopperBulbBlock.java
@@ -34,7 +34,12 @@ public class CopperBulbBlock extends Block implements WeatheringCopper {
 
     @Override
     public int getLightEmission(BlockState state, BlockGetter level, BlockPos pos) {
-        return state.getValue(LIT) ? 15 : 0;
+        if (state.getValue(LIT)) {
+            return 15;
+        }
+        else {
+            return 0;
+        }
     }
 
     @Override
