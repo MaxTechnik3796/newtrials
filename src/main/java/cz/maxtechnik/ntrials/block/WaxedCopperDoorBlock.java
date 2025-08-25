d8d7f2f9dd3e7a6a7bc2e1783ea59033a110af7c
Adam Bartoš <programingmaster147@gmail.com>
2025-08-25 11:56:45 +0200

    remove if colisions from waxed doors

--- src/main/java/cz/maxtechnik/ntrials/block/WaxedCopperDoorBlock.java
+++ src/main/java/cz/maxtechnik/ntrials/block/WaxedCopperDoorBlock.java
@@ -1,7 +1,6 @@
 package cz.maxtechnik.ntrials.block;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
@@ -18,20 +17,10 @@ import net.minecraft.world.level.block.DoorBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockSetType;
-import net.minecraft.world.level.block.state.properties.DoorHingeSide;
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.phys.shapes.VoxelShape;
-import net.minecraft.world.phys.shapes.Shapes;
 import cz.maxtechnik.ntrials.NTrialsModEvents;
 import org.jetbrains.annotations.NotNull;
 
 public class WaxedCopperDoorBlock extends DoorBlock {
-    // Shape definitions for different door orientations when closed
-    protected static final VoxelShape SOUTH_AABB = box(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 3.0D);
-    protected static final VoxelShape NORTH_AABB = box(0.0D, 0.0D, 13.0D, 16.0D, 16.0D, 16.0D);
-    protected static final VoxelShape WEST_AABB = box(13.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D);
-    protected static final VoxelShape EAST_AABB = box(0.0D, 0.0D, 0.0D, 3.0D, 16.0D, 16.0D);
 
     public WaxedCopperDoorBlock(BlockBehaviour.Properties props){
         super(props, BlockSetType.OAK);
@@ -40,30 +29,6 @@ public class WaxedCopperDoorBlock extends DoorBlock {
     @Override
     public int getLightBlock(@NotNull BlockState state,@NotNull BlockGetter worldIn,@NotNull BlockPos pos){
         return 0;
-    }
-
-    @Override
-    public @NotNull VoxelShape getShape(@NotNull BlockState state, @NotNull BlockGetter level, @NotNull BlockPos pos, @NotNull CollisionContext context) {
-        Direction direction = state.getValue(FACING);
-        boolean flag = !state.getValue(OPEN);
-        boolean flag1 = state.getValue(HINGE) == DoorHingeSide.RIGHT;
-
-        return switch (direction) {
-            case SOUTH -> flag ? SOUTH_AABB : (flag1 ? EAST_AABB : WEST_AABB);
-            case WEST -> flag ? WEST_AABB : (flag1 ? SOUTH_AABB : NORTH_AABB);
-            case EAST -> flag ? EAST_AABB : (flag1 ? NORTH_AABB : SOUTH_AABB);
-            default -> flag ? SOUTH_AABB : (flag1 ? WEST_AABB : EAST_AABB);
-        };
-    }
-
-    @Override
-    public @NotNull VoxelShape getCollisionShape(@NotNull BlockState state, @NotNull BlockGetter level, @NotNull BlockPos pos, @NotNull CollisionContext context) {
-        // When the door is open, there should be no collision
-        if (state.getValue(OPEN)) {
-            return Shapes.empty();
-        }
-        // When closed, use the same shape as the visual shape
-        return getShape(state, level, pos, context);
     }
 
     @Override
