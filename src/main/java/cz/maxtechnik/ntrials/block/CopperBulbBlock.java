d74184427be0a7647ac3d6854bc5903e8bb39a2b
Adam Bartoš <programingmaster147@gmail.com>
2025-08-26 10:17:50 +0200

    scraping func

--- src/main/java/cz/maxtechnik/ntrials/block/CopperBulbBlock.java
+++ src/main/java/cz/maxtechnik/ntrials/block/CopperBulbBlock.java
@@ -124,6 +124,26 @@ public class CopperBulbBlock extends Block implements WeatheringCopper {
                 return InteractionResult.sidedSuccess(level.isClientSide);
             }
         }
+        if (itemInHand.getItem()instanceof AxeItem){
+            Block scrapedBlock=NTrialsModEvents.SCRAPING_MAP.get(this);
+            if(scrapedBlock!=null){ // Null znamená že je to první fáze (nelze čistit dál)
+                if(!level.isClientSide){
+                    level.setBlock(pos,scrapedBlock.defaultBlockState(),3);
+                    level.playSound(null,pos,SoundEvents.AXE_SCRAPE,SoundSource.BLOCKS,1.0f,1.0f);
+                    if(level instanceof ServerLevel serverLevel){
+                        for(int i=0;i<20;i++){
+                            double x=pos.getX()-0.2+level.random.nextDouble()*1.4;
+                            double y=pos.getY()-0.2+level.random.nextDouble()*1.4;
+                            double z=pos.getZ()-0.2+level.random.nextDouble()*1.4;
+                            serverLevel.sendParticles(ParticleTypes.SCRAPE,x,y,z,1,0,0,0,0.05);
+                        }
+                    }
+                    // Poškodenie nástroja
+                    itemInHand.hurtAndBreak(1,player,(p)->p.broadcastBreakEvent(hand));
+                }
+                return InteractionResult.sidedSuccess(level.isClientSide);
+            }
+        }
 
         return super.use(state,level,pos,player,hand,hit);
     }
