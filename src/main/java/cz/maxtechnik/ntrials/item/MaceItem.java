e4f9c2a26f52fc3e3a8b6418adb2bd6cab8d98d8
Adam Bartoš <programingmaster147@gmail.com>
2025-08-29 14:35:53 +0200

    delete fake effects

--- src/main/java/cz/maxtechnik/ntrials/item/MaceItem.java
+++ src/main/java/cz/maxtechnik/ntrials/item/MaceItem.java
@@ -72,17 +72,8 @@ public class MaceItem extends SwordItem {
                     Vec3 knockback = target.position().subtract(player.position()).normalize().scale(knockbackStrength);
                     target.setDeltaMovement(target.getDeltaMovement().add(knockback.x, 0.3, knockback.z));
 
-                    // Stun efekt při velmi silném úderu
-                    if (bonusDamage > 8.0f) {
-                        int stunDuration = (int) Math.min(60, 20 + (bonusDamage * 2));
-                        int stunLevel = bonusDamage > 15.0f ? 3 : 2;
-                        target.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, stunDuration, stunLevel));
-                    }
 
-                    // Critické efekty při extrémní výšce
-                    if (fallDistance > 20.0f) {
-                        target.addEffect(new MobEffectInstance(MobEffects.BLINDNESS, 60, 0));
-                    }
+
                 }
 
                 // Reset fall distance po útoku
