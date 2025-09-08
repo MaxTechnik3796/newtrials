8c2d5a598b8e3983c15afaa2bc1bf4616108e819
Adam Bartoš <programingmaster147@gmail.com>
2025-09-08 17:52:03 +0200

    entity animation

--- src/main/java/cz/maxtechnik/ntrials/block/TrialSpawnerBlock.java
+++ src/main/java/cz/maxtechnik/ntrials/block/TrialSpawnerBlock.java
@@ -2,7 +2,17 @@ package cz.maxtechnik.ntrials.block;
 
 import cz.maxtechnik.ntrials.block.entity.TrialSpawnerBlockEntity;
 import cz.maxtechnik.ntrials.init.NTrialsModBlockEntities;
+import cz.maxtechnik.ntrials.network.NetworkHandler;
+import cz.maxtechnik.ntrials.network.TrialSpawnerSyncPacket;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.SpawnEggItem;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
@@ -18,6 +28,8 @@ import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.material.PushReaction;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.network.PacketDistributor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -28,6 +40,37 @@ public class TrialSpawnerBlock extends BaseEntityBlock{
     public TrialSpawnerBlock(){
         super(Properties.of().sound(SoundType.METAL).strength(-1,3600000).noOcclusion().mapColor(MapColor.COLOR_BLACK).isRedstoneConductor((bs,br,bp)->false).noLootTable().pushReaction(PushReaction.BLOCK));
         this.registerDefaultState(this.stateDefinition.any().setValue(OMINOUS,false).setValue(STATE,TrialSpawnerState.INACTIVE));
+    }
+
+    @Override
+    public @NotNull InteractionResult use(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos, @NotNull Player player, @NotNull InteractionHand hand, @NotNull BlockHitResult hit) {
+        ItemStack itemStack = player.getItemInHand(hand);
+
+        if (itemStack.getItem() instanceof SpawnEggItem spawnEggItem) {
+            if (!level.isClientSide) {
+                BlockEntity blockEntity = level.getBlockEntity(pos);
+                if (blockEntity instanceof TrialSpawnerBlockEntity trialSpawner) {
+                    EntityType<?> entityType = spawnEggItem.getType(itemStack.getTag());
+                    trialSpawner.setSpawnEntity(entityType);
+
+                    // Synchronize to all players in range
+                    if (level instanceof ServerLevel serverLevel) {
+                        TrialSpawnerSyncPacket packet = new TrialSpawnerSyncPacket(pos, entityType);
+                        NetworkHandler.INSTANCE.send(PacketDistributor.TRACKING_CHUNK.with(() ->
+                            serverLevel.getChunkAt(pos)), packet);
+                    }
+
+                    if (!player.isCreative()) {
+                        itemStack.shrink(1);
+                    }
+
+                    return InteractionResult.SUCCESS;
+                }
+            }
+            return InteractionResult.sidedSuccess(level.isClientSide);
+        }
+
+        return InteractionResult.PASS;
     }
 
     @Override
@@ -42,11 +85,10 @@ public class TrialSpawnerBlock extends BaseEntityBlock{
 
     @Override
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(@NotNull Level level, @NotNull BlockState state, @NotNull BlockEntityType<T> blockEntityType) {
-        if (level.isClientSide()) {
-            return null;
-        }
         return createTickerHelper(blockEntityType, NTrialsModBlockEntities.TRIAL_SPAWNER_BLOCK_ENTITY.get(),
-                (level1, pos, state1, blockEntity) -> blockEntity.tick());
+                level.isClientSide()
+                    ? (level1, pos, state1, blockEntity) -> blockEntity.clientTick()
+                    : (level1, pos, state1, blockEntity) -> blockEntity.tick());
     }
 
     public enum TrialSpawnerState implements net.minecraft.util.StringRepresentable {
--- src/main/java/cz/maxtechnik/ntrials/block/entity/TrialSpawnerBlockEntity.java
+++ src/main/java/cz/maxtechnik/ntrials/block/entity/TrialSpawnerBlockEntity.java
@@ -3,9 +3,13 @@ package cz.maxtechnik.ntrials.block.entity;
 import cz.maxtechnik.ntrials.init.NTrialsModBlockEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -16,6 +20,8 @@ public class TrialSpawnerBlockEntity extends BlockEntity {
     private int cooldownTime = 0;
     private boolean isOminous = false;
     private int tickCount = 0;
+    private int clientTickCount = 0; // Client-side tick counter pro animace
+    private EntityType<?> spawnEntity = null; // Tag pro uložení entity typu
 
     public TrialSpawnerBlockEntity(BlockPos pos, BlockState blockState) {
         super(NTrialsModBlockEntities.TRIAL_SPAWNER_BLOCK_ENTITY.get(), pos, blockState);
@@ -32,12 +38,25 @@ public class TrialSpawnerBlockEntity extends BlockEntity {
         // This is where you would handle spawning, state transitions, etc.
     }
 
+    public void clientTick() {
+        this.clientTickCount++;
+    }
+
     @Override
     public void load(@NotNull CompoundTag tag) {
         super.load(tag);
         this.cooldownTime = tag.getInt("CooldownTime");
         this.isOminous = tag.getBoolean("Ominous");
         this.tickCount = tag.getInt("TickCount");
+
+        // Load spawn entity
+        if (tag.contains("SpawnEntity")) {
+            String entityTypeId = tag.getString("SpawnEntity");
+            ResourceLocation entityLocation = new ResourceLocation(entityTypeId);
+            this.spawnEntity = ForgeRegistries.ENTITY_TYPES.getValue(entityLocation);
+        } else {
+            this.spawnEntity = null;
+        }
 
         // Load detected players
         if (tag.contains("DetectedPlayers")) {
@@ -60,6 +79,14 @@ public class TrialSpawnerBlockEntity extends BlockEntity {
         tag.putInt("CooldownTime", this.cooldownTime);
         tag.putBoolean("Ominous", this.isOminous);
         tag.putInt("TickCount", this.tickCount);
+
+        // Save spawn entity
+        if (this.spawnEntity != null) {
+            ResourceLocation entityLocation = ForgeRegistries.ENTITY_TYPES.getKey(this.spawnEntity);
+            if (entityLocation != null) {
+                tag.putString("SpawnEntity", entityLocation.toString());
+            }
+        }
 
         // Save detected players
         CompoundTag playersTag = new CompoundTag();
@@ -94,6 +121,24 @@ public class TrialSpawnerBlockEntity extends BlockEntity {
 
     public int getTickCount() {
         return tickCount;
+    }
+
+    public int getClientTickCount() {
+        return clientTickCount;
+    }
+
+    @Nullable
+    public EntityType<?> getSpawnEntity() {
+        return spawnEntity;
+    }
+
+    public void setSpawnEntity(@Nullable EntityType<?> spawnEntity) {
+        this.spawnEntity = spawnEntity;
+        setChanged();
+    }
+
+    public boolean hasSpawnEntity() {
+        return this.spawnEntity != null;
     }
 
     public void addDetectedPlayer(UUID playerId) {
