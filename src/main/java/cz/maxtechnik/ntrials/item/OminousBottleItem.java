2a08e51e59d0e05ce7689e43b114f2ff041ef97b
MaxTechnik <maxtechnik.oficial@gmail.com>
2025-10-12 21:29:52 +0200

    random items added

---
+++ src/main/java/cz/maxtechnik/ntrials/item/OminousBottleItem.java
@@ -0,0 +1,9 @@
+package cz.maxtechnik.ntrials.item;
+
+import net.minecraft.world.item.Item;
+
+public class OminousBottleItem extends Item {
+    public OminousBottleItem(){
+        super(new Properties());
+    }
+}
