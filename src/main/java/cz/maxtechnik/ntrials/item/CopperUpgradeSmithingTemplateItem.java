fea44c7d0a2c81ff429e25cff81dd91353a542cf
MaxTechnik <maxtechnik.oficial@gmail.com>
2025-10-14 16:16:51 +0200

    copper upgrade part 2 & OminousBottleItem

--- src/main/java/cz/maxtechnik/ntrials/item/CopperUpgradeSmithingTemplateItem.java
+++ src/main/java/cz/maxtechnik/ntrials/item/CopperUpgradeSmithingTemplateItem.java
@@ -1,9 +1,28 @@
 package cz.maxtechnik.ntrials.item;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.CommonComponents;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class CopperUpgradeSmithingTemplateItem extends Item {
     public CopperUpgradeSmithingTemplateItem(){
         super(new Properties());
+    }
+    @Override
+    public void appendHoverText(@NotNull ItemStack itemstack,Level level,@NotNull List<Component>list,@NotNull TooltipFlag flag){
+        super.appendHoverText(itemstack,level,list,flag);
+        list.add(Component.translatable("upgrade.ntrials.copper_upgrade").withStyle(ChatFormatting.GRAY));
+        list.add(CommonComponents.EMPTY);
+        list.add(Component.translatable("item.minecraft.smithing_template.applies_to").withStyle(ChatFormatting.GRAY));
+        list.add(CommonComponents.space().append(Component.translatable("item.ntrials.smithing_template.copper_upgrade.applies_to").withStyle(ChatFormatting.BLUE)));
+        list.add(Component.translatable("item.minecraft.smithing_template.ingredients").withStyle(ChatFormatting.GRAY));
+        list.add(CommonComponents.space().append(Component.translatable("item.ntrials.smithing_template.copper_upgrade.ingredients").withStyle(ChatFormatting.BLUE)));
     }
 }
--- src/main/java/cz/maxtechnik/ntrials/item/OminousBottleItem.java
+++ src/main/java/cz/maxtechnik/ntrials/item/OminousBottleItem.java
@@ -1,9 +1,46 @@
 package cz.maxtechnik.ntrials.item;
 
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public class OminousBottleItem extends Item {
     public OminousBottleItem(){
-        super(new Properties());
+        super(new Properties().stacksTo(16).food(new FoodProperties.Builder().nutrition(0).saturationMod(0).alwaysEat().build()));
     }
+    @Override
+    public @NotNull UseAnim getUseAnimation(@NotNull ItemStack itemstack){
+        return UseAnim.DRINK;
+    }
+    @Override
+    public @NotNull ItemStack finishUsingItem(@NotNull ItemStack itemstack,@NotNull Level world,@NotNull LivingEntity entity){
+        ItemStack retval=new ItemStack(Items.GLASS_BOTTLE);
+        super.finishUsingItem(itemstack,world,entity);
+        if(!(world.isClientSide()))
+            if(entity.hasEffect(MobEffects.BAD_OMEN)){
+                entity.addEffect(new MobEffectInstance(MobEffects.BAD_OMEN,120000,1+Objects.requireNonNull(entity.getEffect(MobEffects.BAD_OMEN)).getAmplifier()));
+            }else{
+                entity.addEffect(new MobEffectInstance(MobEffects.BAD_OMEN,120000,0));
+            }
+        if(itemstack.isEmpty()){
+            return retval;
+        }else{
+            if(entity instanceof Player player&&!player.getAbilities().instabuild){
+                if(!player.getInventory().add(retval))
+                    player.drop(retval,false);
+            }
+            return itemstack;
+        }
+    }
+
 }
