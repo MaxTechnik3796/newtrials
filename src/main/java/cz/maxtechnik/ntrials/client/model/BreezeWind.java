a82915d2693b69b1246499ecf185d582bf854bbf
MaxTechnik <maxtechnik.oficial@gmail.com>
2025-10-31 15:44:35 +0100

    breeze wind texture animation

--- src/main/java/cz/maxtechnik/ntrials/client/model/BreezeWind.java
+++ src/main/java/cz/maxtechnik/ntrials/client/model/BreezeWind.java
@@ -1,7 +1,4 @@
-package cz.maxtechnik.ntrials.client.model;// Made with Blockbench 5.0.3
-// Exported for Minecraft version 1.17 or later with Mojang mappings
-// Paste this class into your mod and generate all required imports
-
+package cz.maxtechnik.ntrials.client.model;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
@@ -11,11 +8,11 @@ import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import org.jetbrains.annotations.NotNull;
 
 public class BreezeWind<T extends Entity> extends EntityModel<T> {
-	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
 	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(ResourceLocation.fromNamespaceAndPath("ntrials", "breeze_wind"), "main");
 	private final ModelPart Wind;
 
@@ -27,21 +24,29 @@ public class BreezeWind<T extends Entity> extends EntityModel<T> {
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
 
-		PartDefinition Wind = partdefinition.addOrReplaceChild("Wind", CubeListBuilder.create().texOffs(1, 83).addBox(-2.5F, -7.0F, -2.5F, 5.0F, 7.0F, 5.0F, new CubeDeformation(0.0F))
-		.texOffs(74, 28).addBox(-6.0F, -13.0F, -6.0F, 12.0F, 6.0F, 12.0F, new CubeDeformation(0.0F))
-		.texOffs(0, 0).addBox(-9.0F, -21.0F, -9.0F, 18.0F, 8.0F, 18.0F, new CubeDeformation(0.0F))
-		.texOffs(6, 6).addBox(-6.0F, -21.0F, -6.0F, 12.0F, 8.0F, 12.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+		PartDefinition Wind = partdefinition.addOrReplaceChild("Wind", CubeListBuilder.create()
+						// První vrstva: nejmenší a nejblíže
+						.texOffs(1, 83).addBox(-2.5F, -7.0F, -2.5F, 5.0F, 7.0F, 5.0F, new CubeDeformation(0.0F))
+						// Druhá vrstva
+						.texOffs(74, 28).addBox(-6.0F, -13.0F, -6.0F, 12.0F, 6.0F, 12.0F, new CubeDeformation(0.0F))
+						// Třetí vrstva: největší a nejvzdálenější
+						.texOffs(0, 0).addBox(-9.0F, -21.0F, -9.0F, 18.0F, 8.0F, 18.0F, new CubeDeformation(0.0F))
+						// Čtvrtá vrstva
+						.texOffs(6, 6).addBox(-6.0F, -21.0F, -6.0F, 12.0F, 8.0F, 12.0F, new CubeDeformation(0.0F)),
+				PartPose.offset(0.0F, 24.0F, 0.0F));
 
 		return LayerDefinition.create(meshdefinition, 128, 128);
 	}
 
 	@Override
-	public void setupAnim(@NotNull Entity entity,float limbSwing,float limbSwingAmount,float ageInTicks,float netHeadYaw,float headPitch) {
-
+	public void setupAnim(@NotNull T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+		// Zde nechte prázdné, model větru nepotřebuje animaci kloubů.
 	}
 
 	@Override
-	public void renderToBuffer(@NotNull PoseStack poseStack,@NotNull VertexConsumer vertexConsumer,int packedLight,int packedOverlay,float red,float green,float blue,float alpha) {
+	public void renderToBuffer(@NotNull PoseStack poseStack, @NotNull VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+		// Pouze renderujeme statický model.
 		Wind.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 	}
 }
+
