db1eeb63e58a7bca8243349eede2a8b960273274
Suriken222 <suriken222a@gmail.com>
2025-11-09 11:46:31 +0100

    Optimalized

--- src/main/java/cz/maxtechnik/ntrials/client/model/BreezeWindBossModel.java
+++ src/main/java/cz/maxtechnik/ntrials/client/model/BreezeWindBossModel.java
@@ -12,6 +12,7 @@ import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
+import org.jetbrains.annotations.NotNull;
 
 public class BreezeWindBossModel<T extends Entity> extends EntityModel<T> {
 	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
@@ -30,23 +31,23 @@ public class BreezeWindBossModel<T extends Entity> extends EntityModel<T> {
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
 
-		PartDefinition wind_top = partdefinition.addOrReplaceChild("wind_top", CubeListBuilder.create().texOffs(0, 0).addBox(-18.0F, -28.0F, -18.0F, 36.0F, 16.0F, 36.0F, new CubeDeformation(0.0F))
-		.texOffs(0, 53).addBox(-13.0F, -28.0F, -13.0F, 25.0F, 16.0F, 26.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 11.0F, 0.0F));
+        partdefinition.addOrReplaceChild("wind_top", CubeListBuilder.create().texOffs(0, 0).addBox(-18.0F, -28.0F, -18.0F, 36.0F, 16.0F, 36.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 53).addBox(-13.0F, -28.0F, -13.0F, 25.0F, 16.0F, 26.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 11.0F, 0.0F));
 
-		PartDefinition wind_middle = partdefinition.addOrReplaceChild("wind_middle", CubeListBuilder.create().texOffs(0, 55).addBox(-12.0F, -19.0F, -12.0F, 24.0F, 12.0F, 24.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 17.0F, 0.0F));
+        partdefinition.addOrReplaceChild("wind_middle", CubeListBuilder.create().texOffs(0, 55).addBox(-12.0F, -19.0F, -12.0F, 24.0F, 12.0F, 24.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 17.0F, 0.0F));
 
-		PartDefinition wind_bottom = partdefinition.addOrReplaceChild("wind_bottom", CubeListBuilder.create().texOffs(0, 116).addBox(-4.5F, -14.0F, -4.5F, 9.0F, 14.0F, 9.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+        partdefinition.addOrReplaceChild("wind_bottom", CubeListBuilder.create().texOffs(0, 116).addBox(-4.5F, -14.0F, -4.5F, 9.0F, 14.0F, 9.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
 
-		return LayerDefinition.create(meshdefinition, 256, 256);
+        return LayerDefinition.create(meshdefinition, 256, 256);
 	}
 
 	@Override
-	public void setupAnim(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+	public void setupAnim(@NotNull Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
 
 	}
 
 	@Override
-	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+	public void renderToBuffer(@NotNull PoseStack poseStack, @NotNull VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
 		wind_top.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 		wind_middle.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 		wind_bottom.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
--- src/main/java/cz/maxtechnik/ntrials/client/renderer/entity/BreezeRenderer.java
+++ src/main/java/cz/maxtechnik/ntrials/client/renderer/entity/BreezeRenderer.java
@@ -81,73 +81,66 @@ public class BreezeRenderer extends MobRenderer<BreezeEntity,BreezeModel> {
 	// --- Vnitřní třída (Wrapper) pro posun UV (Zůstává nezměněna) ---
 	// Tato třída zachytí volání UV a přičte k němu náš posun
 
-	private static class UvScrollingVertexConsumer implements VertexConsumer {
-		private final VertexConsumer parent;
-		private final float uOffset;
+    private record UvScrollingVertexConsumer(VertexConsumer parent, float uOffset) implements VertexConsumer {
 
-		public UvScrollingVertexConsumer(VertexConsumer parent, float uOffset) {
-			this.parent = parent;
-			this.uOffset = uOffset;
-		}
+        @NotNull
+        @Override
+        public VertexConsumer vertex(double x, double y, double z) {
+            parent.vertex(x, y, z);
+            return this;
+        }
 
-		@NotNull
-		@Override
-		public VertexConsumer vertex(double x, double y, double z) {
-			parent.vertex(x, y, z);
-			return this;
-		}
+        @NotNull
+        @Override
+        public VertexConsumer color(int red, int green, int blue, int alpha) {
+            parent.color(red, green, blue, alpha);
+            return this;
+        }
 
-		@NotNull
-		@Override
-		public VertexConsumer color(int red, int green, int blue, int alpha) {
-			parent.color(red, green, blue, alpha);
-			return this;
-		}
+        @NotNull
+        @Override
+        public VertexConsumer uv(float u, float v) {
+            // TOTO JE TO KOUZLO:
+            // K původní U souřadnici (horizontální) přičteme náš posun (uOffset)
+            // U souřadnice automaticky "wrapne" (přeteče) díky geometrii modelu.
+            parent.uv(u + uOffset, v);
+            return this;
+        }
 
-		@NotNull
-		@Override
-		public VertexConsumer uv(float u, float v) {
-			// TOTO JE TO KOUZLO:
-			// K původní U souřadnici (horizontální) přičteme náš posun (uOffset)
-			// U souřadnice automaticky "wrapne" (přeteče) díky geometrii modelu.
-			parent.uv(u + uOffset, v);
-			return this;
-		}
+        @NotNull
+        @Override
+        public VertexConsumer overlayCoords(int u, int v) {
+            parent.overlayCoords(u, v);
+            return this;
+        }
 
-		@NotNull
-		@Override
-		public VertexConsumer overlayCoords(int u, int v) {
-			parent.overlayCoords(u, v);
-			return this;
-		}
+        @NotNull
+        @Override
+        public VertexConsumer uv2(int u, int v) {
+            parent.uv2(u, v);
+            return this;
+        }
 
-		@NotNull
-		@Override
-		public VertexConsumer uv2(int u, int v) {
-			parent.uv2(u, v);
-			return this;
-		}
+        @NotNull
+        @Override
+        public VertexConsumer normal(float x, float y, float z) {
+            parent.normal(x, y, z);
+            return this;
+        }
 
-		@NotNull
-		@Override
-		public VertexConsumer normal(float x, float y, float z) {
-			parent.normal(x, y, z);
-			return this;
-		}
+        @Override
+        public void endVertex() {
+            parent.endVertex();
+        }
 
-		@Override
-		public void endVertex() {
-			parent.endVertex();
-		}
+        @Override
+        public void defaultColor(int defaultRed, int defaultGreen, int defaultBlue, int defaultAlpha) {
+            parent.defaultColor(defaultRed, defaultGreen, defaultBlue, defaultAlpha);
+        }
 
-		@Override
-		public void defaultColor(int defaultRed, int defaultGreen, int defaultBlue, int defaultAlpha) {
-			parent.defaultColor(defaultRed, defaultGreen, defaultBlue, defaultAlpha);
-		}
-
-		@Override
-		public void unsetDefaultColor() {
-			parent.unsetDefaultColor();
-		}
-	}
+        @Override
+        public void unsetDefaultColor() {
+            parent.unsetDefaultColor();
+        }
+    }
 }
