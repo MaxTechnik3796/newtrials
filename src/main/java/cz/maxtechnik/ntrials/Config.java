25942a63fc5931ffd6fae60d4287fc4c038e2cfb
MaxTechnik <maxtechnik.oficial@gmail.com>
2025-11-10 19:56:24 +0100

    .

--- src/main/java/cz/maxtechnik/ntrials/Config.java
+++ src/main/java/cz/maxtechnik/ntrials/Config.java
@@ -10,5 +10,6 @@ public class Config{
     private static final ForgeConfigSpec.Builder BUILDER=new ForgeConfigSpec.Builder();
     static final ForgeConfigSpec SPEC=BUILDER.build();
     @SubscribeEvent
+
     static void onLoad(final ModConfigEvent event){}
 }
