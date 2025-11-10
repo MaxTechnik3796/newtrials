3a294bbcc3f775effb54c13bb7d5c9e87c9e1e52
Suriken222 <suriken222a@gmail.com>
2025-11-10 20:39:58 +0100

    Function 1.0.0

--- src/main/java/cz/maxtechnik/ntrials/init/ModEvents.java
+++ src/main/java/cz/maxtechnik/ntrials/init/ModEvents.java
@@ -2,14 +2,7 @@ package cz.maxtechnik.ntrials.init;
 
 
 import cz.maxtechnik.ntrials.NTrialsMod;
-
-import cz.maxtechnik.ntrials.NTrialsMod; // Nahraďte cestou k vaší hlavní třídě
-import cz.maxtechnik.ntrials.init.ModStructureTags; // <-- DŮLEŽITÝ NOVÝ IMPORT (viz Krok 3)
-
 import net.minecraft.world.entity.npc.VillagerProfession;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraftforge.common.BasicItemListing;
 import net.minecraftforge.event.village.VillagerTradesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -21,8 +14,7 @@ public class ModEvents {
     public static void registerTrades(VillagerTradesEvent event) {
         
         if (event.getType() == VillagerProfession.CARTOGRAPHER) {
-			event.getTrades().get(1).add(new TrialsMapTrade(5, 12, 1));
-            event.getTrades().get(3).add(new BasicItemListing(new ItemStack(Items.EMERALD,5),new ItemStack(Items.MAP,1),new ItemStack(Items.STICK,1),1,10,0.05F));
+			event.getTrades().get(3).add(new TrialsMapTrade(12, 12, 1));
         }
     }
 }
