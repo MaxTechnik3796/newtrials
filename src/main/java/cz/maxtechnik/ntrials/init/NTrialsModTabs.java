0a5e229c6f28e44c3c7d24d30881c3ac0dbfb86a
MaxTechnik <maxtechnik.oficial@gmail.com>
2025-11-16 17:16:30 +0100

    mining & tabs fix

--- src/main/java/cz/maxtechnik/ntrials/init/NTrialsModTabs.java
+++ src/main/java/cz/maxtechnik/ntrials/init/NTrialsModTabs.java
@@ -12,95 +12,91 @@ import net.minecraftforge.registries.RegistryObject;
 public class NTrialsModTabs{
     public static final DeferredRegister<CreativeModeTab>REGISTER=DeferredRegister.create(Registries.CREATIVE_MODE_TAB, NTrialsMod.MODID);
     public static final RegistryObject<CreativeModeTab>MAIN=REGISTER.register("main",()->CreativeModeTab.builder().title(Component.translatable("creative_tab.ntrials.main")).icon(()->new ItemStack(NTrialsModBlocks.TUFF_BRICKS.get().asItem())).displayItems(((parameters,tabData)->{
-
-        tabData.accept(NTrialsModItems.TRIAL_KEY.get());
-        tabData.accept(NTrialsModItems.OMINOUS_TRIAL_KEY.get());
+		tabData.accept(NTrialsModItems.TRIAL_KEY.get());
+		tabData.accept(NTrialsModItems.OMINOUS_TRIAL_KEY.get());
 		tabData.accept(NTrialsModItems.BOSS_TRIAL_KEY.get());
-        tabData.accept(NTrialsModItems.OMINOUS_BOTTLE.get());
-        tabData.accept(NTrialsModItems.COPPER_UPGRADE_SMITHING_TEMPLATE.get());
+		tabData.accept(NTrialsModItems.OMINOUS_BOTTLE.get());
+		tabData.accept(NTrialsModItems.COPPER_UPGRADE_SMITHING_TEMPLATE.get());
 
-        tabData.accept(NTrialsModBlocks.TRIAL_SPAWNER.get().asItem());
-        tabData.accept(NTrialsModBlocks.VAULT.get().asItem());
+		tabData.accept(NTrialsModItems.TRIAL_SPAWNER.get());
+		tabData.accept(NTrialsModItems.VAULT.get());
 
 		tabData.accept(NTrialsModItems.TRIAL_SPAWNER_BOSS.get());
 		tabData.accept(NTrialsModItems.VAULT_BOSS.get());
 
-        tabData.accept(NTrialsModItems.BOGGED_SPAWN_EGG.get());
-        tabData.accept(NTrialsModItems.BREEZE_SPAWN_EGG.get());
+		tabData.accept(NTrialsModItems.BOGGED_SPAWN_EGG.get());
+		tabData.accept(NTrialsModItems.BREEZE_SPAWN_EGG.get());
 		tabData.accept(NTrialsModItems.BREEZE_BOSS_SPAWN_EGG.get());
 
-        tabData.accept(NTrialsModItems.WIND_CHARGE.get());
-        tabData.accept(NTrialsModItems.BREEZE_ROD.get());
-        tabData.accept(NTrialsModBlocks.HEAVY_CORE.get().asItem());
-        tabData.accept(NTrialsModItems.MACE.get());
-        tabData.accept(NTrialsModItems.MACE_HANDLE.get());
+		tabData.accept(NTrialsModItems.WIND_CHARGE.get());
+		tabData.accept(NTrialsModItems.BREEZE_ROD.get());
+		tabData.accept(NTrialsModItems.HEAVY_CORE.get());
+		tabData.accept(NTrialsModItems.MACE.get());
+		tabData.accept(NTrialsModItems.MACE_HANDLE.get());
 
+		tabData.accept(NTrialsModItems.TUFF_BRICKS.get());
+		tabData.accept(NTrialsModItems.CHISELED_TUFF.get());
+		tabData.accept(NTrialsModItems.CHISELED_TUFF_BRICKS.get());
+		tabData.accept(NTrialsModItems.POLISHED_TUFF.get());
 
-        tabData.accept(NTrialsModBlocks.TUFF_BRICKS.get().asItem());
-        tabData.accept(NTrialsModBlocks.CHISELED_TUFF.get().asItem());
-        tabData.accept(NTrialsModBlocks.CHISELED_TUFF_BRICKS.get().asItem());
-        tabData.accept(NTrialsModBlocks.POLISHED_TUFF.get().asItem());
+		tabData.accept(NTrialsModItems.TUFF_STAIRS.get());
+		tabData.accept(NTrialsModItems.TUFF_BRICK_STAIRS.get());
+		tabData.accept(NTrialsModItems.POLISHED_TUFF_STAIRS.get());
 
-        tabData.accept(NTrialsModBlocks.TUFF_STAIRS.get().asItem());
-        tabData.accept(NTrialsModBlocks.TUFF_BRICK_STAIRS.get().asItem());
-        tabData.accept(NTrialsModBlocks.POLISHED_TUFF_STAIRS.get().asItem());
+		tabData.accept(NTrialsModItems.TUFF_SLAB.get());
+		tabData.accept(NTrialsModItems.TUFF_BRICK_SLAB.get());
+		tabData.accept(NTrialsModItems.POLISHED_TUFF_SLAB.get());
 
-        tabData.accept(NTrialsModBlocks.TUFF_SLAB.get().asItem());
-        tabData.accept(NTrialsModBlocks.TUFF_BRICK_SLAB.get().asItem());
-        tabData.accept(NTrialsModBlocks.POLISHED_TUFF_SLAB.get().asItem());
-
-        tabData.accept(NTrialsModBlocks.TUFF_WALL.get().asItem());
-        tabData.accept(NTrialsModBlocks.TUFF_BRICK_WALL.get().asItem());
-        tabData.accept(NTrialsModBlocks.POLISHED_TUFF_WALL.get().asItem());
+		tabData.accept(NTrialsModItems.TUFF_WALL.get());
+		tabData.accept(NTrialsModItems.TUFF_BRICK_WALL.get());
+		tabData.accept(NTrialsModItems.POLISHED_TUFF_WALL.get());
 
     })).build());
     public static final RegistryObject<CreativeModeTab>COPPER=REGISTER.register("copper",()-> CreativeModeTab.builder().title(Component.translatable("creative_tab.ntrials.copper")).icon(()->new ItemStack(Blocks.COPPER_BLOCK.asItem())).displayItems(((parameters, tabData)->{
 
-        tabData.accept(NTrialsModBlocks.CHISELED_COPPER.get().asItem());
-        tabData.accept(NTrialsModBlocks.EXPOSED_CHISELED_COPPER.get().asItem());
-        tabData.accept(NTrialsModBlocks.WEATHERED_CHISELED_COPPER.get().asItem());
-        tabData.accept(NTrialsModBlocks.OXIDIZED_CHISELED_COPPER.get().asItem());
-        tabData.accept(NTrialsModBlocks.WAXED_CHISELED_COPPER.get().asItem());
-        tabData.accept(NTrialsModBlocks.WAXED_EXPOSED_CHISELED_COPPER.get().asItem());
-        tabData.accept(NTrialsModBlocks.WAXED_WEATHERED_CHISELED_COPPER.get().asItem());
-        tabData.accept(NTrialsModBlocks.WAXED_OXIDIZED_CHISELED_COPPER.get().asItem());
+		tabData.accept(NTrialsModItems.CHISELED_COPPER.get());
+		tabData.accept(NTrialsModItems.EXPOSED_CHISELED_COPPER.get());
+		tabData.accept(NTrialsModItems.WEATHERED_CHISELED_COPPER.get());
+		tabData.accept(NTrialsModItems.OXIDIZED_CHISELED_COPPER.get());
+		tabData.accept(NTrialsModItems.WAXED_CHISELED_COPPER.get());
+		tabData.accept(NTrialsModItems.WAXED_EXPOSED_CHISELED_COPPER.get());
+		tabData.accept(NTrialsModItems.WAXED_WEATHERED_CHISELED_COPPER.get());
+		tabData.accept(NTrialsModItems.WAXED_OXIDIZED_CHISELED_COPPER.get());
 
-        tabData.accept(NTrialsModBlocks.COPPER_DOOR.get().asItem());
-        tabData.accept(NTrialsModBlocks.EXPOSED_COPPER_DOOR.get().asItem());
-        tabData.accept(NTrialsModBlocks.WEATHERED_COPPER_DOOR.get().asItem());
-        tabData.accept(NTrialsModBlocks.OXIDIZED_COPPER_DOOR.get().asItem());
-        tabData.accept(NTrialsModBlocks.WAXED_COPPER_DOOR.get().asItem());
-        tabData.accept(NTrialsModBlocks.WAXED_EXPOSED_COPPER_DOOR.get().asItem());
-        tabData.accept(NTrialsModBlocks.WAXED_WEATHERED_COPPER_DOOR.get().asItem());
-        tabData.accept(NTrialsModBlocks.WAXED_OXIDIZED_COPPER_DOOR.get().asItem());
+		tabData.accept(NTrialsModItems.COPPER_DOOR.get());
+		tabData.accept(NTrialsModItems.EXPOSED_COPPER_DOOR.get());
+		tabData.accept(NTrialsModItems.WEATHERED_COPPER_DOOR.get());
+		tabData.accept(NTrialsModItems.OXIDIZED_COPPER_DOOR.get());
+		tabData.accept(NTrialsModItems.WAXED_COPPER_DOOR.get());
+		tabData.accept(NTrialsModItems.WAXED_EXPOSED_COPPER_DOOR.get());
+		tabData.accept(NTrialsModItems.WAXED_WEATHERED_COPPER_DOOR.get());
+		tabData.accept(NTrialsModItems.WAXED_OXIDIZED_COPPER_DOOR.get());
 
-        tabData.accept(NTrialsModBlocks.COPPER_TRAPDOOR.get().asItem());
-        tabData.accept(NTrialsModBlocks.EXPOSED_COPPER_TRAPDOOR.get().asItem());
-        tabData.accept(NTrialsModBlocks.WEATHERED_COPPER_TRAPDOOR.get().asItem());
-        tabData.accept(NTrialsModBlocks.OXIDIZED_COPPER_TRAPDOOR.get().asItem());
-        tabData.accept(NTrialsModBlocks.WAXED_COPPER_TRAPDOOR.get().asItem());
-        tabData.accept(NTrialsModBlocks.WAXED_EXPOSED_COPPER_TRAPDOOR.get().asItem());
-        tabData.accept(NTrialsModBlocks.WAXED_WEATHERED_COPPER_TRAPDOOR.get().asItem());
-        tabData.accept(NTrialsModBlocks.WAXED_OXIDIZED_COPPER_TRAPDOOR.get().asItem());
+		tabData.accept(NTrialsModItems.COPPER_TRAPDOOR.get());
+		tabData.accept(NTrialsModItems.EXPOSED_COPPER_TRAPDOOR.get());
+		tabData.accept(NTrialsModItems.WEATHERED_COPPER_TRAPDOOR.get());
+		tabData.accept(NTrialsModItems.OXIDIZED_COPPER_TRAPDOOR.get());
+		tabData.accept(NTrialsModItems.WAXED_COPPER_TRAPDOOR.get());
+		tabData.accept(NTrialsModItems.WAXED_EXPOSED_COPPER_TRAPDOOR.get());
+		tabData.accept(NTrialsModItems.WAXED_WEATHERED_COPPER_TRAPDOOR.get());
+		tabData.accept(NTrialsModItems.WAXED_OXIDIZED_COPPER_TRAPDOOR.get());
 
-        tabData.accept(NTrialsModBlocks.COPPER_GRATE.get().asItem());
-        tabData.accept(NTrialsModBlocks.EXPOSED_COPPER_GRATE.get().asItem());
-        tabData.accept(NTrialsModBlocks.WEATHERED_COPPER_GRATE.get().asItem());
-        tabData.accept(NTrialsModBlocks.OXIDIZED_COPPER_GRATE.get().asItem());
-        tabData.accept(NTrialsModBlocks.WAXED_COPPER_GRATE.get().asItem());
-        tabData.accept(NTrialsModBlocks.WAXED_EXPOSED_COPPER_GRATE.get().asItem());
-        tabData.accept(NTrialsModBlocks.WAXED_WEATHERED_COPPER_GRATE.get().asItem());
-        tabData.accept(NTrialsModBlocks.WAXED_OXIDIZED_COPPER_GRATE.get().asItem());
+		tabData.accept(NTrialsModItems.COPPER_GRATE.get());
+		tabData.accept(NTrialsModItems.EXPOSED_COPPER_GRATE.get());
+		tabData.accept(NTrialsModItems.WEATHERED_COPPER_GRATE.get());
+		tabData.accept(NTrialsModItems.OXIDIZED_COPPER_GRATE.get());
+		tabData.accept(NTrialsModItems.WAXED_COPPER_GRATE.get());
+		tabData.accept(NTrialsModItems.WAXED_EXPOSED_COPPER_GRATE.get());
+		tabData.accept(NTrialsModItems.WAXED_WEATHERED_COPPER_GRATE.get());
+		tabData.accept(NTrialsModItems.WAXED_OXIDIZED_COPPER_GRATE.get());
 
-        tabData.accept(NTrialsModBlocks.COPPER_BULB.get().asItem());
-        tabData.accept(NTrialsModBlocks.EXPOSED_COPPER_BULB.get().asItem());
-        tabData.accept(NTrialsModBlocks.WEATHERED_COPPER_BULB.get().asItem());
-        tabData.accept(NTrialsModBlocks.OXIDIZED_COPPER_BULB.get().asItem());
-        tabData.accept(NTrialsModBlocks.WAXED_COPPER_BULB.get().asItem());
-        tabData.accept(NTrialsModBlocks.WAXED_EXPOSED_COPPER_BULB.get().asItem());
-        tabData.accept(NTrialsModBlocks.WAXED_WEATHERED_COPPER_BULB.get().asItem());
-        tabData.accept(NTrialsModBlocks.WAXED_OXIDIZED_COPPER_BULB.get().asItem());
-
+		tabData.accept(NTrialsModItems.COPPER_BULB.get());
+		tabData.accept(NTrialsModItems.EXPOSED_COPPER_BULB.get());
+		tabData.accept(NTrialsModItems.WEATHERED_COPPER_BULB.get());
+		tabData.accept(NTrialsModItems.OXIDIZED_COPPER_BULB.get());
+		tabData.accept(NTrialsModItems.WAXED_COPPER_BULB.get());
+		tabData.accept(NTrialsModItems.WAXED_EXPOSED_COPPER_BULB.get());
+		tabData.accept(NTrialsModItems.WAXED_WEATHERED_COPPER_BULB.get());
+		tabData.accept(NTrialsModItems.WAXED_OXIDIZED_COPPER_BULB.get());
     })).build());
-
 }
