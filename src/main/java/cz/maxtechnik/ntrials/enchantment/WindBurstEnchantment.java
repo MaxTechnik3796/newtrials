94a3c787f78760bafc69340b0bfdc261b4816dc9
MaxTechnik <maxtechnik.oficial@gmail.com>
2025-12-11 17:24:31 +0100

    enchant fix

--- src/main/java/cz/maxtechnik/ntrials/enchantment/WindBurstEnchantment.java
+++ src/main/java/cz/maxtechnik/ntrials/enchantment/WindBurstEnchantment.java
@@ -1,27 +1,23 @@
 package cz.maxtechnik.ntrials.enchantment;
 
+import cz.maxtechnik.ntrials.item.MaceItem;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
-
-public class WindBurstEnchantment extends Enchantment {
-
-    public WindBurstEnchantment() {
-        super(Rarity.RARE, EnchantmentCategory.WEAPON, new EquipmentSlot[]{EquipmentSlot.MAINHAND});
-    }
-
-    @Override
-    public int getMinCost(int level) {
-        return 15 + (level - 1) * 9;
-    }
-
-    @Override
-    public int getMaxCost(int level) {
-        return this.getMinCost(level) + 50;
-    }
-
-    @Override
-    public int getMaxLevel() {
-        return 3;
-    }
+public class WindBurstEnchantment extends Enchantment{
+	public WindBurstEnchantment(){
+		super(Rarity.VERY_RARE,EnchantmentCategory.create("mace",item->item instanceof MaceItem),new EquipmentSlot[]{EquipmentSlot.MAINHAND});
+	}
+	@Override
+	public int getMinCost(int level){
+		return 15+(level-1)*9;
+	}
+	@Override
+	public int getMaxCost(int level){
+		return this.getMinCost(level)+50;
+	}
+	@Override
+	public int getMaxLevel(){
+		return 3;
+	}
 }
